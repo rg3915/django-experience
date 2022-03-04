1fc1d39f33f70416126bba8bb2ddbdd896bd199f
Regis Santos <regis42santos@gmail.com>
2022-03-04 07:20:30 -0300

    Editando as mensagens de erro. close #11

---
+++ backend/core/handler.py
@@ -0,0 +1,17 @@
+from rest_framework import status
+from rest_framework.views import exception_handler
+
+
+def custom_exception_handler(exc, context):
+    response = exception_handler(exc, context)
+    method = context['request'].method
+
+    if response.status_code == status.HTTP_403_FORBIDDEN:
+        if method == 'POST':
+            response.data = {'message': 'Você não tem permissão para Adicionar.'}
+        elif method == 'PUT' or method == 'PATCH':
+            response.data = {'message': 'Você não tem permissão para Editar.'}
+        elif method == 'DELETE':
+            response.data = {'message': 'Você não tem permissão para Deletar.'}
+
+    return response
--- backend/settings.py
+++ backend/settings.py
@@ -65,6 +65,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'EXCEPTION_HANDLER': 'backend.core.handler.custom_exception_handler',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
