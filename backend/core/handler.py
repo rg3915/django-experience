df994545e87966aedc97c142cacd55e2ef0c0e7b
Regis Santos <regis42santos@gmail.com>
2022-04-22 04:50:09 -0300

    bugfix: AttributeError: 'NoneType' object has no attribute 'status_code'.

--- backend/core/handler.py
+++ backend/core/handler.py
@@ -6,12 +6,13 @@ def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
     method = context['request'].method
 
-    if response.status_code == status.HTTP_403_FORBIDDEN:
-        if method == 'POST':
-            response.data = {'message': 'Você não tem permissão para Adicionar.'}
-        elif method == 'PUT' or method == 'PATCH':
-            response.data = {'message': 'Você não tem permissão para Editar.'}
-        elif method == 'DELETE':
-            response.data = {'message': 'Você não tem permissão para Deletar.'}
+    if response:
+        if response.status_code == status.HTTP_403_FORBIDDEN:
+            if method == 'POST':
+                response.data = {'message': 'Você não tem permissão para Adicionar.'}
+            elif method == 'PUT' or method == 'PATCH':
+                response.data = {'message': 'Você não tem permissão para Editar.'}
+            elif method == 'DELETE':
+                response.data = {'message': 'Você não tem permissão para Deletar.'}
 
-    return response
+        return response
